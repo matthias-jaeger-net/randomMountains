9a0c0ca6e0b53be0aae3ad5507f3a546280bc445
Matthias Jäger <write@matthias-jaeger.net>
2018-04-07 01:49:49 +0200

    color trys

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 
 function setup() {
    createCanvas(windowWidth, windowHeight);
+
+   // Image Effects using a p5 image object
    var img = createImage(800, 800);
    img.loadPixels();
    for(x = 0; x < img.width; x++) {
       for(y = 0; y < img.height; y++) {
          a = map(y, 0, img.height, 255, 0);
-         img.set(x, y, [0, random(20, 80), a, random(20, 80)]);
+         img.set(x, y, [a, random(20, 80), a, random(20, 80)]);
+         // A beautiful error
+         // img.set(x, y, [0, random(20, 80), a, random(20, 80)]);
       }
    }
    img.updatePixels();
    background(img);
 
-   fill(10, 0, random(100, 255), 20);
+   //fill(10, 0, random(100, 255), 20);
    noStroke();
 
    var lines = [];
@@ -27,11 +31,23 @@ function setup() {
 
    total = lines.length;
    while (total--) {
+      fill(random(255), random(255), map(total, 0, lines.length, 100, 30), 30);
       noiseShape(lines[total][0], lines[total][1], random(100, 300), random(100, 200), random(0.02, 0.1));
    }
+}
 
-   fill(0, 10);
-   noiseShape();
+function draw() {
+
+   fill(100, 20, 100, 8);
+   var darstart = createVector(-100, random(0, height));
+   var darkend = createVector(width + 100, random(height/2, height));
+   noiseShape(darstart, darkend, 26, 100, random(0.03, 0.73));
+
+
+   fill(0, 20, 40, 8);
+   var darstart = createVector(-100, random(height/2, height-100));
+   var darkend = createVector(width + 10, random(height/2, height));
+   noiseShape(darstart, darkend, 19, 900, random(0.1, 0.3));
 }
 
 /*
